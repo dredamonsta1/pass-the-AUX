89f32f4355d369d8612b13cb72619900850b0f67
Andre Wilkinson <andre_w3@yahoo.com>
2025-06-13 12:25:40 -0400

    update src redux store.tsx

--- frontend/src/redux/store.tsx
+++ frontend/src/redux/store.tsx
@@ -26,11 +26,44 @@
 
 //*****************NEW*************** 
 
-// src/redux/store.tsx
+// // src/redux/store.tsx
+// import { configureStore } from "@reduxjs/toolkit";
+// import artistsReducer from "./reducers/artistsReducer";
+// // Import authReducer
+// import authReducer from "./store/authSlice"; // Assuming authSlice.tsx is in src/store/authSlice.tsx
+
+
+// // Configure the Redux store
+// const store = configureStore({
+//   reducer: {
+//     // 'artists' is the slice of state managed by artistsReducer
+//     artists: artistsReducer,
+//     // Add authReducer here
+//     auth: authReducer,
+//   },
+//   // configureStore automatically sets up Redux Thunk, which we need for async actions.
+// });
+
+// // Infer the `RootState` type from the store itself
+// // This type represents the entire Redux state tree
+// export type RootState = ReturnType<typeof store.getState>;
+
+// // Infer the `AppDispatch` type from the store's dispatch method
+// // This type helps in correctly typing the dispatch function, especially for async thunks
+// export type AppDispatch = typeof store.dispatch;
+
+// // Export the configured store as the default export
+// export default store;
+
+
+
+//***************************New ******************
+
+
 import { configureStore } from "@reduxjs/toolkit";
 import artistsReducer from "./reducers/artistsReducer";
-// Import authReducer
-import authReducer from "./store/authSlice"; // Assuming authSlice.tsx is in src/store/authSlice.tsx
+// Corrected Import: Assuming authSlice.tsx is in src/store/authSlice.tsx
+import authReducer from "../store/authSlice";
 
 
 // Configure the Redux store
